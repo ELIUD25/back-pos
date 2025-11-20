64935d03e4d8819b865cf80d7137e3fbffbb1f2b
ELIUD25 <ichigoeliud021@gmail.com>
2025-11-20 09:10:19 +0300

    Update

--- server.js
+++ server.js
@@ -877,7 +877,8 @@ app.use((req, res, next) => {
 app.use(compression());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  // origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'https://seridah-chemist.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 }));
